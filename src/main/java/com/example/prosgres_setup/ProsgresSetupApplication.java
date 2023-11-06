71c6c3abb9a987cdc7c5f322f97e114f5c42da96
Rex <rezrex999@gmail.com>
2023-11-06 20:22:06 +0600

    init project

---
+++ src/main/java/com/example/prosgres_setup/ProsgresSetupApplication.java
@@ -0,0 +1,13 @@
+package com.example.prosgres_setup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProsgresSetupApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProsgresSetupApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/prosgres_setup/ProsgresSetupApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.prosgres_setup;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProsgresSetupApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
